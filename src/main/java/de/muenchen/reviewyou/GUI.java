4fa1225677a7c1856aa69badf5dcad4a10542a17
leberkassepp <77366628+leberkassepp@users.noreply.github.com>
2022-05-02 15:00:21 +0200

    Update GUI.java
    added some labels and stuff

--- src/main/java/de/muenchen/reviewyou/GUI.java
+++ src/main/java/de/muenchen/reviewyou/GUI.java
@@ -1,4 +1,27 @@
 package de.muenchen.reviewyou;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GUI {
+
+
+    private Container contentPane;
+    private JFrame window;
+
+
+    // First page to get user data
+
+    private JLabel name;
+    private JLabel email;
+    private JLabel date;
+    private JLabel telephone;
+    private JTextField instructorName;
+    private JTextField instructorEmail;
+    private JTextField currentDate;
+    private JTextField instructorTelephone;
+
+    public void startFrame(){
+        
+    }
 }
