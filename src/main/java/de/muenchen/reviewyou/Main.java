93e4126786fef9f8a450ca5d7a3d1ae1b1ad8b0b
rinorahmetaj <97606972+rinorahmetaj@users.noreply.github.com>
2022-05-02 14:22:05 +0200

    created branch

--- src/main/java/de/muenchen/reviewyou/Main.java
+++ src/main/java/de/muenchen/reviewyou/Main.java
@@ -3,5 +3,6 @@ package de.muenchen.reviewyou;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hi");
     }
 }
