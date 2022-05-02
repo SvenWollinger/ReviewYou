594b0e3cff6296e5305ce167585ffa9c65e65365
Coldstor <93476610+Coldstor@users.noreply.github.com>
2022-05-02 14:51:29 +0200

    Client

--- src/main/java/de/muenchen/reviewyou/Socket/Socket.java
+++ src/main/java/de/muenchen/reviewyou/Socket/Socket.java
@@ -1,8 +1,47 @@
 package de.muenchen.reviewyou.Socket;
 
+
+
+import java.io.*;
 import java.net.*;
-import java.net.*;
+
 
 public class Socket {
     //TODO: Send the created HTML to browser
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public class GreetClient {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public void startConnection(String ip, int port) {
+            clientSocket = new Socket(ip, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
+
+        public String sendMessage(String msg) {
+            out.println(msg);
+            String resp = in.readLine();
+            return resp;
+        }
+
+        public void stopConnection() {
+            in.close();
+            out.close();
+            clientSocket.close();
+        }
+    }
 }
