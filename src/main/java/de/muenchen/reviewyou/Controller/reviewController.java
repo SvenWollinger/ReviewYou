696acb4471f490a04fcd2bffba7a164121ec699d
HerrMaximus <m.loenker@icloud.com>
2022-05-03 11:08:06 +0200

    Added our task and first two ActionListener

---
+++ src/main/java/de/muenchen/reviewyou/Controller/reviewController.java
@@ -0,0 +1,27 @@
+package de.muenchen.reviewyou.Controller;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class reviewController {
+    //TODO: We make the actionListener for UI group
+    //TODO: At the end is a "weiter" button. If you click the button, the inserted data gets safed. On every page (expect first page) is a "zurück" button
+    //TODO: If you click on "zurück" the safed data from every JTextField gets inserted again into their previous TextField
+
+    ActionListener actionListenerNextPage = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //
+        }
+    };
+
+    ActionListener actionListenerPreviousPage = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //
+        }
+    };
+
+
+}
