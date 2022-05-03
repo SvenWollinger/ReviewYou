500b0bc36a96142ad05d8f07b7777e9dd8260051
LeoBaumann98 <97606203+LeoBaumann98@users.noreply.github.com>
2022-05-03 11:11:46 +0200

    Tried some things with the Xssf Sheet and it didnt work ...

--- src/main/java/de/muenchen/reviewyou/ExcelHandler.java
+++ src/main/java/de/muenchen/reviewyou/ExcelHandler.java
@@ -1,24 +1,37 @@
 package de.muenchen.reviewyou;
 import org.apache.poi.*;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.*;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 
 public class ExcelHandler {
 
-    File myFile = new File("C:\\Users\\Leonhard Baumann\\Downloads\\1_Überarbeitet.xlsm");
-    FileInputStream fis = new FileInputStream(myFile);
+    File myFile = new File("C:\\Users\\Leonhard Baumann\\Documents\\GitHub\\ReviewYou\\resources\\1_Überarbeitet.xlsx");
+    InputStream fis = new FileInputStream(myFile);
     XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fis);
     XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
 
 
     public ExcelHandler() throws IOException {
+    }
+
+
+    public boolean writeCell(int identifier, String text) throws IOException {
+        switch(identifier) {
+            case 1:
+                Cell cell = null;
+                cell = xssfSheet.getRow(0).getCell(55);
+                cell.setCellValue(text);
+                fis.close();
+                OutputStream outputStream = new FileOutputStream("C:\\Users\\Leonhard Baumann\\Documents\\GitHub\\ReviewYou\\resources\\1_Überarbeitet.xlsx");
+                xssfWorkbook.write(outputStream);
+                outputStream.close();
+        }
+        return true;
     }
 }
--- src/main/java/de/muenchen/reviewyou/Main.java
+++ src/main/java/de/muenchen/reviewyou/Main.java
@@ -1,8 +1,13 @@
 package de.muenchen.reviewyou;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
         System.out.println("Hi");
-    }
+
+        ExcelHandler excelHandler = new ExcelHandler();
+        excelHandler.writeCell(1, "Baumann Leonhard");
+            }
 }
