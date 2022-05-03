18aa73f0dac7b08f79905223522bd13bb4b0e97a
Coldstor <93476610+Coldstor@users.noreply.github.com>
2022-05-03 13:31:21 +0200

    create Getter

--- src/main/java/de/muenchen/reviewyou/Controller/data.java
+++ src/main/java/de/muenchen/reviewyou/Controller/data.java
@@ -27,18 +27,3 @@ public class data {
 
 
     //Setter and Getter
-    public String getFrage1() { return frage1; }
-    public int getFrage1Punkte() { return frage1Punkte; }
-
-    public String getFrage2() { return frage2; }
-    public int getFrage2Punkte() { return frage2Punkte; }
-
-    public String getFrage3() { return frage3; }
-    public int getFrage3Punkte() { return frage3Punkte; }
-
-    public String getFrage4() { return frage4; }
-    public int getFrage4Punkte() { return frage4Punkte; }
-
-    public String getFrage5() { return frage5; }
-    public int getFrage5Punkte() { return frage5Punkte; }
-}
