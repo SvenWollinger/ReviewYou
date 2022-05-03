f7a35b3e444d020f07703121773f7626e1014b11
Coldstor <93476610+Coldstor@users.noreply.github.com>
2022-05-03 13:32:29 +0200

    create Getter

--- src/main/java/de/muenchen/reviewyou/Controller/data.java
+++ src/main/java/de/muenchen/reviewyou/Controller/data.java
@@ -3,16 +3,6 @@ package de.muenchen.reviewyou.Controller;
 import java.time.LocalDate;
 
 public class data {
-    private String frage1;
-    private int frage1Punkte;
-    private String frage2;
-    private int frage2Punkte;
-    private String frage3;
-    private int frage3Punkte;
-    private String frage4;
-    private int frage4Punkte;
-    private String frage5;
-    private int frage5Punkte;
 
 
     private String firstName, lastName, street, houseNumber, postalCode, location, course, intershipSection,
