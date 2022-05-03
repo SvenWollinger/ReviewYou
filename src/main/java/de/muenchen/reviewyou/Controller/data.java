8f7450902d60afafa5ac5dacdaee8148739474f4
HerrMaximus <m.loenker@icloud.com>
2022-05-03 11:19:12 +0200

    Create data.java

---
+++ src/main/java/de/muenchen/reviewyou/Controller/data.java
@@ -0,0 +1,8 @@
+package de.muenchen.reviewyou.Controller;
+
+public class data {
+    private String x;
+    //TODO: Of course we have more Strings, add them later!
+
+    //TODO: They also make a rating bar, we have to safe it aswell
+}
