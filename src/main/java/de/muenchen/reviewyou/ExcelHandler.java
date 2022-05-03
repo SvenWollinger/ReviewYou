98f3037905d422168efb3bef18aa071abe439cca
LeoBaumann98 <97606203+LeoBaumann98@users.noreply.github.com>
2022-05-03 11:40:46 +0200

    added excel file

--- src/main/java/de/muenchen/reviewyou/ExcelHandler.java
+++ src/main/java/de/muenchen/reviewyou/ExcelHandler.java
@@ -12,7 +12,7 @@ import java.io.*;
 public class ExcelHandler {
 
     File myFile = new File("C:\\Users\\Leonhard Baumann\\Documents\\GitHub\\ReviewYou\\resources\\1_Überarbeitet.xlsx");
-    InputStream fis = new FileInputStream(myFile);
+    FileInputStream fis = new FileInputStream(myFile);
     XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fis);
     XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
 
@@ -24,11 +24,10 @@ public class ExcelHandler {
     public boolean writeCell(int identifier, String text) throws IOException {
         switch(identifier) {
             case 1:
-                Cell cell = null;
-                cell = xssfSheet.getRow(0).getCell(55);
+                Cell cell = xssfSheet.getRow(55).getCell(0);
                 cell.setCellValue(text);
                 fis.close();
-                OutputStream outputStream = new FileOutputStream("C:\\Users\\Leonhard Baumann\\Documents\\GitHub\\ReviewYou\\resources\\1_Überarbeitet.xlsx");
+                FileOutputStream outputStream = new FileOutputStream("1_Überarbeitet.xlsx");
                 xssfWorkbook.write(outputStream);
                 outputStream.close();
         }
