719ba9ebaf028f8c9870581a95613514a4fc8df8
LeoBaumann98 <97606203+LeoBaumann98@users.noreply.github.com>
2022-05-03 11:50:13 +0200

    Change how we load xlsx file
    File is now loaded from resources directory

--- src/main/java/de/muenchen/reviewyou/ExcelHandler.java
+++ src/main/java/de/muenchen/reviewyou/ExcelHandler.java
@@ -7,27 +7,38 @@ import org.apache.poi.xssf.usermodel.*;
 
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 
 public class ExcelHandler {
 
-    File myFile = new File("C:\\Users\\Leonhard Baumann\\Documents\\GitHub\\ReviewYou\\resources\\1_Überarbeitet.xlsx");
-    FileInputStream fis = new FileInputStream(myFile);
-    XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fis);
-    XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
+    File myFile = new File("FinalFile.xlsx");
+    FileInputStream fis;
+    XSSFWorkbook xssfWorkbook;
+    XSSFSheet xssfSheet;
 
 
     public ExcelHandler() throws IOException {
+        InputStream src = ExcelHandler.class.getResourceAsStream("/Gradingtable.xlsx");
+        Files.copy(src, Paths.get(myFile.getName()), StandardCopyOption.REPLACE_EXISTING);
     }
 
 
     public boolean writeCell(int identifier, String text) throws IOException {
+        if(fis == null || xssfWorkbook == null ||xssfSheet == null) {
+            fis = new FileInputStream(myFile);
+            xssfWorkbook = new XSSFWorkbook(fis);
+            xssfSheet = xssfWorkbook.getSheetAt(0);
+        }
+
         switch(identifier) {
             case 1:
                 Cell cell = xssfSheet.getRow(55).getCell(0);
                 cell.setCellValue(text);
                 fis.close();
-                FileOutputStream outputStream = new FileOutputStream("1_Überarbeitet.xlsx");
+                FileOutputStream outputStream = new FileOutputStream(myFile.getName());
                 xssfWorkbook.write(outputStream);
                 outputStream.close();
         }
