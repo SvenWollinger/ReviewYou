cf668afb1ca304d9dde1faf891216a497910b25a
HerrMaximus <m.loenker@icloud.com>
2022-05-03 11:21:20 +0200

    First Setter/Getter and instruction

--- src/main/java/de/muenchen/reviewyou/Controller/data.java
+++ src/main/java/de/muenchen/reviewyou/Controller/data.java
@@ -1,8 +1,17 @@
 package de.muenchen.reviewyou.Controller;
 
 public class data {
-    private String x;
-    //TODO: Of course we have more Strings, add them later!
+    private String name;
+    //TODO: Of course we have more Strings, add them + Getter/Setter later!
 
     //TODO: They also make a rating bar, we have to safe it aswell
+
+    //Setter and Getter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
