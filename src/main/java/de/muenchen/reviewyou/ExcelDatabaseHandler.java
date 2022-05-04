512b9f94691498495f22e7fa50240b5fafb6d777
george-ctrl <57451553+george-ctrl@users.noreply.github.com>
2022-05-04 08:28:39 +0200

    Updated Excel database handler

--- src/main/java/de/muenchen/reviewyou/ExcelDatabaseHandler.java
+++ src/main/java/de/muenchen/reviewyou/ExcelDatabaseHandler.java
@@ -1,10 +1,52 @@
 package de.muenchen.reviewyou;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
 import java.util.ArrayList;
 
 public class ExcelDatabaseHandler {
+
+
+    File myFile = new File("/Gradingtable.xlsx");
+    FileInputStream fis;
+    XSSFWorkbook xssfWorkbook;
+    XSSFSheet xssfSheet;
+
+
     private int[][] grades = new int[15][19];
-    private ArrayList<String> possiblesForGerman;
+    private static ExcelDatabaseHandler excelDatabaseHandler = new ExcelDatabaseHandler();
+
+
+    public static ExcelDatabaseHandler getInstance(){
+        return excelDatabaseHandler;
+    }
+
+    private ExcelDatabaseHandler(){
+        InputStream src = ExcelHandler.class.getResourceAsStream("/Gradingtable.xlsx");
+
+    }
+
+    public void addRecommendation(String id1, String id2, String recommendation) throws IOException {
+        if(fis == null || xssfWorkbook == null ||xssfSheet == null) {
+            fis = new FileInputStream(myFile);
+            xssfWorkbook = new XSSFWorkbook(fis);
+            xssfSheet = xssfWorkbook.getSheetAt(1);
+        }
+        int lastRow = xssfSheet.getLastRowNum();
+        xssfSheet.createRow(++lastRow).getCell(0).setCellValue(id1);
+        xssfSheet.getRow(++lastRow).getCell(1).setCellValue(id2);
+        xssfSheet.getRow(++lastRow).getCell(2).setCellValue(recommendation);
+    }
+
+
+
+
+
+
+
+/*    private ArrayList<String> possiblesForGerman;
     private ArrayList<String> possiblesForIt;
     private ArrayList<String> possiblesForInterest;
     private ArrayList<String> possiblesForAnalyticalThinking;
@@ -19,7 +61,16 @@ public class ExcelDatabaseHandler {
     private ArrayList<String> possiblesForStressTolerance;
     private ArrayList<String> possiblesForIdentification;
     private ArrayList<String> possiblesForIndependence;
-    private ArrayList<String> possiblesForCrtics;
+    private ArrayList<String> possiblesForCrtics;*/
+
+
+    private String[][] irgendwas;
+
+    //TODO ARRAY IDENTIFIER 1 WHICH QUESTION ?? SO 0-18
+    //TODO ARRAY IDENTIFIER 2 WHICH GRADE?? 0-5
+    //TODO ARRAY MESSAGE STRINGS
+
+
 
 
 
--- src/main/java/de/muenchen/reviewyou/Main.java
+++ src/main/java/de/muenchen/reviewyou/Main.java
@@ -8,13 +8,17 @@ public class Main {
         System.out.println("Hi");
 
         ExcelHandler excelHandler = new ExcelHandler();
-        excelHandler.writeStudentData("Baumann Leonhard", "30.04.1998", "Geheim", "1", "Schnitzel");
+        ExcelDatabaseHandler excelDatabaseHandler = ExcelDatabaseHandler.getInstance();
+
+/*        excelHandler.writeStudentData("Baumann Leonhard", "30.04.1998", "Geheim", "1", "Schnitzel");
         excelHandler.writePerformance("Stinkt","Stinkt","Mehr Stinken","Stinker", "Stinkt gut");
         excelHandler.writeAllocationPeriod("10.10.10", "11.11.11", "Dusche");
         excelHandler.writeDates("10.10.10", "11.10.10");
         excelHandler.writeTrainingAreaAndPeriod("Dusche und von heute bis gestern");
         excelHandler.writeParticipations("BlahBlah sagt Bumbo in the Schrein");
         excelHandler.writePoints(133, 10);
-        excelHandler.writeTotalandAverage("Geeignet", "13");
+        excelHandler.writeTotalandAverage("Geeignet", "13");*/
+        excelDatabaseHandler.addRecommendation("5", "7","Leo stinkt manchmal ganz schoen");
+
             }
 }
