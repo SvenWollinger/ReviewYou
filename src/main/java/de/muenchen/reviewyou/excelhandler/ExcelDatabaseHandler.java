7b008b633f578f87affdeffc5b8545869462567a
george-ctrl <57451553+george-ctrl@users.noreply.github.com>
2022-05-04 13:43:19 +0200

    removed redundant code

--- src/main/java/de/muenchen/reviewyou/excelhandler/ExcelDatabaseHandler.java
+++ src/main/java/de/muenchen/reviewyou/excelhandler/ExcelDatabaseHandler.java
@@ -18,8 +18,7 @@ public class ExcelDatabaseHandler {
     XSSFWorkbook xssfWorkbook;
     XSSFSheet xssfSheet;
 
-
-    private int[][] grades = new int[15][19];
+    
     private static ExcelDatabaseHandler excelDatabaseHandler;
 
     static {
@@ -77,52 +76,9 @@ public class ExcelDatabaseHandler {
     }
 
 
-
-
-
-
-
-
-
-/*    private ArrayList<String> possiblesForGerman;
-    private ArrayList<String> possiblesForIt;
-    private ArrayList<String> possiblesForInterest;
-    private ArrayList<String> possiblesForAnalyticalThinking;
-    private ArrayList<String> possiblesForConnectedThinking;
-    private ArrayList<String> possiblesForCommunication;
-    private ArrayList<String> possiblesForContacts;
-    private ArrayList<String> possiblesForEmpathy;
-    private ArrayList<String> possiblesForNotRacist;
-    private ArrayList<String> possiblesForOpeness;
-    private ArrayList<String> possiblesForIntegrity;
-    private ArrayList<String> possiblesForMotivation;
-    private ArrayList<String> possiblesForStressTolerance;
-    private ArrayList<String> possiblesForIdentification;
-    private ArrayList<String> possiblesForIndependence;
-    private ArrayList<String> possiblesForCrtics;*/
-
-
-    private String[][] irgendwas;
-
     //TODO ARRAY IDENTIFIER 1 WHICH QUESTION ?? SO 0-18
     //TODO ARRAY IDENTIFIER 2 WHICH GRADE?? 0-5
     //TODO ARRAY MESSAGE STRINGS
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public void setGrade(int i, int j, int grade) {
-        this.grades[i][j] = grade;
-
     }
-}
+
