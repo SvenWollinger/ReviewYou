bb2b8331890615c89f6135595f2bac81fd292cfa
george-ctrl <57451553+george-ctrl@users.noreply.github.com>
2022-05-05 08:50:05 +0200

    Removed stuff

--- src/main/java/de/muenchen/reviewyou/excelhandler/Main.java
+++ src/main/java/de/muenchen/reviewyou/excelhandler/Main.java
@@ -12,14 +12,15 @@ public class Main {
 
 
 /*      excelHandler.writeStudentData("Baumann Leonhard", "30.04.1998", "Geheim", "1", "Schnitzel");
-        excelHandler.writePerformance("Stinkt","Stinkt","Mehr Stinken","Stinker", "Stinkt gut");
+        excelHandler.writePerformance("Redet","Singt","Tanzt","Lehrer", "Sowieso");
         excelHandler.writeAllocationPeriod("10.10.10", "11.11.11", "Dusche");
         excelHandler.writeDates("10.10.10", "11.10.10");
         excelHandler.writeTrainingAreaAndPeriod("Dusche und von heute bis gestern");
         excelHandler.writeParticipations("BlahBlah sagt Bumbo in the Schrein");
         excelHandler.writePoints(133, 10);
         excelHandler.writeTotalandAverage("Geeignet", "13");*/
-        excelDatabaseHandler.addRecommendation(5, 7,"Leo stinkt manchmal ganz schoen");
+
+        excelDatabaseHandler.addRecommendation(5, 7,"Kann Gut Deutsch");
         System.out.println(excelDatabaseHandler.createRecListFromExcel());
     }
 }
