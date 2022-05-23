52a48f69cc852a11163cae50410e16cc52f2637d
george-ctrl <57451553+george-ctrl@users.noreply.github.com>
2022-05-23 16:07:07 +0200

    Added Azubi DataStruc
    AzubiData Structs and Generator(Arraylist)

---
+++ src/main/java/de/muenchen/reviewyou/excelhandler/Azubi.java
@@ -0,0 +1,29 @@
+package de.muenchen.reviewyou.excelhandler;
+
+import java.time.LocalDate;
+
+public class Azubi {
+   private String name;
+   private LocalDate birthday;
+   private String address;
+   private int year;
+   private String course;
+   private LocalDate allocationPeriodFrom;
+   private LocalDate allocationPeriodTo;
+
+
+   public Azubi(String name, LocalDate birthday, String address, int year, String course, LocalDate allocationPeriodFrom, LocalDate allocationPeriodTo) {
+      this.name = name;
+      this.birthday = birthday;
+      this.address = address;
+      this.year = year;
+      this.course = course;
+      this.allocationPeriodFrom = allocationPeriodFrom;
+      this.allocationPeriodTo = allocationPeriodTo;
+   }
+
+
+
+
+
+}
---
+++ src/main/java/de/muenchen/reviewyou/excelhandler/AzubiGenerator.java
@@ -0,0 +1,43 @@
+package de.muenchen.reviewyou.excelhandler;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AzubiGenerator {
+
+   private static Object AzubiGenerator;
+   InputStream is = ClassLoader.getSystemResourceAsStream("/Gradingtable.xlsx");
+   File file = new File("src/main/resources/Gradingtable.xlsx");
+   FileInputStream fis;
+   XSSFWorkbook xssfWorkbook;
+   XSSFSheet xssfSheet;
+
+   public List<Azubi> createAzubiListFromExcel() throws IOException {
+      if(fis == null || xssfWorkbook == null ||xssfSheet == null) {
+         fis = new FileInputStream(file);
+         xssfWorkbook = new XSSFWorkbook(fis);
+         xssfSheet = xssfWorkbook.getSheetAt(1);
+      }
+      int lastRow = xssfSheet.getLastRowNum();
+      List<Azubi> list = new ArrayList<>();
+      for (int i = 1; i < lastRow; i++) {
+         Row row = xssfSheet.getRow(i);
+         list.add(new Azubi(
+               row.getCell(0).getStringCellValue(),
+               row.getCell(1).getLocalDateTimeCellValue().toLocalDate(),
+               row.getCell(2).getStringCellValue(),
+               (int)row.getCell(3).getNumericCellValue(),
+               row.getCell(4).getStringCellValue(),
+               row.getCell(5).getLocalDateTimeCellValue().toLocalDate(),
+               row.getCell(6).getLocalDateTimeCellValue().toLocalDate()));
+      }
+      return list;
+   }
+}
