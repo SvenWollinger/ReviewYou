e7564fd9e52f8f41b7723da8f218188228c866f6
Coldstor <93476610+Coldstor@users.noreply.github.com>
2022-05-24 14:34:31 +0200

    Made useful stuff

--- src/main/java/de/muenchen/reviewyou/Controller/reviewController.java
+++ src/main/java/de/muenchen/reviewyou/Controller/reviewController.java
@@ -1,12 +1,33 @@
 package de.muenchen.reviewyou.Controller;
 
+import jdk.internal.access.JavaIOFileDescriptorAccess;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 public class reviewController {
     //Dummies
+    int question1Points;
+    int question2Points;
+    int question3Points;
+    int question4Points;
+    int
+    int question8Points;
+    int question9Points;
+    int question10Points;
+    int question11Points;
+    int question12Points;
+    int question13Points;
+    int question14Points;
+    int question15Points;
+    int question16Points;
+    int question17Points;
+    int question18Points;
+    int question19Points;
+    double totalscore;
+    String OverallAssessment;
+
     JButton calc = new JButton();
     JButton saveAndNew = new JButton();
     JButton saveAndExit = new JButton();
@@ -33,20 +54,18 @@ public class reviewController {
     JTextArea developements = new JTextArea();
     JTextArea perspective = new JTextArea();
     JTextArea others = new JTextArea();
-    JLabel jLabelTotal = new JLabel();
-    JLabel jLabelAverage = new JLabel();
-    private List<JSlider> jSliders = createSliders();
 
     public reviewController() {
+
+        //TODO: Ask Excel-Group if the code is good + how should we do the 2x big tables? + what does writePoints?
+        //TODO: + tell them that we have E-Mail now
 
         ActionListener actionListenerSafeData = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 traineeRating traineeRating = new traineeRating();
 
-                //Give values to Excel-Group
-                writeInstructorData(instructorName.getText(), instructorTelephone.getText(), currentDate.getText(),
-                        instructorEmail.getText());
+                writeInstructorData(instructorName.getText(), instructorTelephone.getText(), currentDate.getText(), instructorEmail.getText());
                 writeStudentData(txtTraineeName.getText(), txtBirthDate.getText(), txtApartmentStreet.getText(),
                         txtTraineeYear.getText(), txtCourse.getText());
                 writeAllocationPeriod(txtFrom.getText(), txtTill.getText(), txtIntershipSelection.getText());
@@ -55,21 +74,15 @@ public class reviewController {
                 writeDates(txtTrainingsPlan.getText(), txtInternimTalk.getText());
                 writePerformance(traineeRating.abilities(), traineeRating.strength(), traineeRating.developments(),
                         traineeRating.perspective(), traineeRating.others());
-                writeTotalandAverage(jLabelTotal.getText(), jLabelAverage.getText());
 
                 if (e.getSource().equals(saveAndNew)) {
-                    for(int i = 0; i < 19; i++) {
-                        //Reset every slider
-                        jSliders.get(i).setValue(jSliders.get(i).getMinimum());
-                    }
+                    //TODO: Need Josef´s code for the slider reset
 
-                    //Clear text
                     String placeholder = "";
                     txtTraineeName.setText(placeholder);
                     txtBirthDate.setText(placeholder);
                     txtApartmentStreet.setText(placeholder);
 
-                    //Go to page 1
                     panel1.removeAll();
                     panel1.revalidate();
                     panel1.repaint();
@@ -77,7 +90,6 @@ public class reviewController {
                     panel.revalidate();
                     panel.repaint();
                     startPanel();
-
                 } else if (e.getSource().equals(saveAndExit)) {
                     System.exit(0);
                 }
@@ -85,15 +97,25 @@ public class reviewController {
         };
 
 
-        //Calculate-button
         ActionListener actionListenerCalculate = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                jLabelTotal.setText(String.valueOf(calculateTotalPoints()));
-                jLabelAverage.setText(calculateAverage());
+                //Button "Berechnen"
+                //Calculate "Punktzahl" and "Gesamturteil" -> writeTotalandAverage() (Josef)
+                double totalscore = ( question1Points + question2Points + question3Points + question4Points + question5Points +
+                                        question6Points + question7Points + question8Points + question9Points + question10Points +
+                                        question11Points + question12Points + question13Points + question14Points + question15Points +
+                                        question16Points + question17Points + question18Points + question19Points) / 19;
+
             }
         };
+        if(totalscore > 8){
+            String OverallAssessment = "Geeignet";
 
+        }else{
+            String OverallAssessment = "Ungeeignet";
+        }
+)
 
         //Dummies for add jButton to actionListener
         saveAndNew.addActionListener(actionListenerSafeData);
@@ -101,7 +123,7 @@ public class reviewController {
         calc.addActionListener(actionListenerSafeData);
     }
 
-    public void textForTraineeRating(traineeRating traineeRating) { //TODO: Ask Excel how they did it
+    public void textForTraineeRating(traineeRating traineeRating) {
         abilities.setText(traineeRating.abilities());
         strength.setText(traineeRating.strength());
         developements.setText(traineeRating.developments());
@@ -109,38 +131,78 @@ public class reviewController {
         others.setText(traineeRating.others());
     }
 
-    public String calculateAverage() {
-        String average = "";
-        for(int i = 0; i < calculateTotalPoints(); i++) {
-            if(calculateTotalPoints() < 100 && calculateTotalPoints() > 90) {
-                average = "xxx";
-            }
-        }
-
-        //TODO: Output a text based on the values from sliders
-        return average;
-    }
-
-    public int calculateTotalPoints() {
-        int totalPoints = 0;
-        for(int i = 0; i < 19; i++) {
-            totalPoints = totalPoints + jSliders.get(i).getValue();
-        }
-        return totalPoints;
-    }
-
     //Dummies
-    public void writeInstructorData(String name, String telephone, String date, String eMail) {}
-    public void writeStudentData(String name, String birthdate, String address, String year, String course) {}
-    public void writeAllocationPeriod(String from, String to, String internshipSection) {}
-    public void writeTrainingAreaAndPeriod(String text) {}
-    public void writeParticipations(String coursesEtc) {}
-    public void writeDates(String trainingPlan, String interimTalk) {}
-    public void writeTotalandAverage(String total, String average) {}
-    public void writePerformance(String abilities, String strengths, String development, String perspectives,
-                                 String other) {}
-    public void startPanel() {}
-    public List<JSlider> createSliders(){
-        return null;
+    public void writeInstructorData(String name, String telephone, String date, String eMail) {
     }
+
+    public void writeStudentData(String name, String birthdate, String address, String year, String course) {
+    }
+
+    public void writeAllocationPeriod(String from, String to, String internshipSection) {
+    }
+
+    public void writeTrainingAreaAndPeriod(String text) {
+    }
+
+    public void writeParticipations(String coursesEtc) {
+    }
+
+    public void writeDates(String trainingPlan, String interimTalk) {
+    }
+
+    public void writeTotalandAverage(String total, String average) {
+
+    }
+
+    public void writePerformance(String abilities, String strengths, String development, String perspectives, String other) {
+    }
+
+    public void startPanel() {
+    }
+
+    ActionListener actionListenercontinue= new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int question1Points = jSliders.get(0).getValue();
+            int question2Points = jSliders.get(1).getValue();
+            int question3Points = jSliders.get(2).getValue();
+        }
+    };
+    ActionListener actionListenercontinue2 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int question4Points = jSliders.get(3).getValue();
+            int question5Points = jSliders.get(4).getValue();
+            int question6Points = jSliders.get(5).getValue();
+        }
+    };
+    ActionListener actionListenercontinue3 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int question7Points = jSliders.get(6).getValue();
+            int question8Points = jSliders.get(7).getValue();
+            int question9Points = jSliders.get(8).getValue();
+            int question10Points = jSliders.get(9).getValue();
+            int question11Points = jSliders.get(10).getValue();
+            int question12Points = jSliders.get(11).getValue();
+        }
+    };
+    ActionListener actionListenercontinue4 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int question13Points = jSliders.get(12).getValue();
+            int question14Points = jSliders.get(13).getValue();
+            int question15Points = jSliders.get(14).getValue();
+            int question16Points = jSliders.get(15).getValue();
+            int question17Points = jSliders.get(16).getValue();
+            int question18Points = jSliders.get(17).getValue();
+            int question19Points = jSliders.get(18).getValue();
+        }
+    };
+
+
+
+
+
+
 }
