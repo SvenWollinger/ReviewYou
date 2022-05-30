fbbbfd6c037fa912a5573847e8382dcabe386f21
george-ctrl <57451553+george-ctrl@users.noreply.github.com>
2022-05-30 11:27:28 +0200

    Revert "jajalassmich"
    This reverts commit 872ac412ef33178817b2e28330a8e410f9b069de, reversing
    changes made to 5d104eb7574ebefb5d9c99da87ae6c45ab712dd5.

---
+++ src/main/java/de/muenchen/reviewyou/Controller/traineeRating.java
@@ -0,0 +1,21 @@
+package de.muenchen.reviewyou.Controller;
+
+public class traineeRating {
+    //TODO: Wait until Olli writes back
+
+    public String abilities() {
+        return "";
+    }
+    public String strength() {
+        return "";
+    }
+    public String developments() {
+        return "";
+    }
+    public String perspective() {
+        return "";
+    }
+    public String others() {
+        return "";
+    }
+}
