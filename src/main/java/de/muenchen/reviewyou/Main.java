d3863bf8a78eb61aadc44b85e1e9ae0b901db0ef
HerrMaximus <m.loenker@icloud.com>
2022-05-31 10:13:28 +0200

    Add azubiGenerator to main

--- src/main/java/de/muenchen/reviewyou/Main.java
+++ src/main/java/de/muenchen/reviewyou/Main.java
@@ -2,14 +2,9 @@ package de.muenchen.reviewyou;
 
 import de.muenchen.reviewyou.Controller.reviewController;
 import de.muenchen.reviewyou.GUI.GUI;
-import de.muenchen.reviewyou.excelhandler.AnswerSuggestionModel;
-import de.muenchen.reviewyou.excelhandler.Azubi;
-import de.muenchen.reviewyou.excelhandler.ExcelDatabaseHandler;
-import de.muenchen.reviewyou.excelhandler.ExcelHandler;
-
+import de.muenchen.reviewyou.excelhandler.*;
 import javax.swing.*;
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -24,6 +19,6 @@ public class Main {
         } catch (UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
-        reviewController reviewController = new reviewController(new ExcelHandler(), new GUI(), ExcelDatabaseHandler.getInstance());
+        reviewController reviewController = new reviewController(new ExcelHandler(), new GUI(), ExcelDatabaseHandler.getInstance(), new AzubiGenerator());
     }
 }
