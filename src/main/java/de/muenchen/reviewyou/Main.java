4c21bb6050c4c4a5aba022e673fd71d0b16f7ae1
LeoBaumann98 <97606203+LeoBaumann98@users.noreply.github.com>
2022-05-31 08:12:48 +0200

    Added set Look and Feel
    Now it looks AMAZING

--- src/main/java/de/muenchen/reviewyou/Main.java
+++ src/main/java/de/muenchen/reviewyou/Main.java
@@ -7,11 +7,17 @@ import de.muenchen.reviewyou.excelhandler.Azubi;
 import de.muenchen.reviewyou.excelhandler.ExcelDatabaseHandler;
 import de.muenchen.reviewyou.excelhandler.ExcelHandler;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         GUI gui = new GUI();
         reviewController reviewController = new reviewController(new ExcelHandler(), new GUI(), ExcelDatabaseHandler.getInstance());
     }
