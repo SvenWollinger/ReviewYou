502b8c059b5c28d2980fc66ce806f9595fd8d832
LeoBaumann98 <97606203+LeoBaumann98@users.noreply.github.com>
2022-07-11 13:25:00 +0200

    change filechooser to native

--- src/main/java/de/muenchen/reviewyou/GUI/GUI.java
+++ src/main/java/de/muenchen/reviewyou/GUI/GUI.java
@@ -486,6 +486,7 @@ public class GUI {
         others.setText("");
         txtPoints.setText("");
         txtReview.setText("");
+        apprenticeshipSelector.setSelectedItem(null);
 
         genderGroup.clearSelection();
         supportColleaguesGroup.clearSelection();
